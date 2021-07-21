43575d06f7a03bc7e8acd304d88e72ef03f8d4fb
maih1 <hoangmai.tq2000@gmail.com>
2021-07-22 02:04:14 +0700

    create dfa and update gitinore

--- DFA.py
+++ DFA.py
@@ -1,1 +1,10 @@
 import Data
+import Automaton as at
+import Data as dt
+
+def dfa(startState, finalStates, states, alphabet, transitions):
+    pass
+automaton = at.automatonData('test1.txt')
+automaton.printAutomation()
+
+a = at.findTransitions('p0', automaton.transitions, automaton.alphabet)
