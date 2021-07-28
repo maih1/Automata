4b6ad6bec412daa0435e1cc99d3ab521d958359d
maih1 <hoangmai.tq2000@gmail.com>
2021-07-29 03:34:12 +0700

    add data and Removal of Unit Productions

--- CNF.py
+++ CNF.py
@@ -1,3 +1,4 @@
+import copy as cp
 import InputCNF as ICNF
 
 # tach chuoi
@@ -47,10 +48,11 @@ def delSym(split_string, sym_eps):
     
     return new_ls_left
 
+# Removal of Null Productions
 # Xóa quy tắc rỗng
-def delEmptyRule(rules):
+def rmNullRules(rules):
     new_rules = []
-    rules = ICNF.setType(rules)
+    # rules = ICNF.setType(rules)
     # left side
     left_side_eps = []
 
@@ -60,6 +62,9 @@ def delEmptyRule(rules):
         if 'epsilon' in i[1]:
             left_side_eps.append(i[0])
             i[1].remove('epsilon')
+        elif len(i[1]) == 0 and i[0] not in left_side_eps:
+            left_side_eps.append(i[0])
+
     # tim cac phan tu khac cua tap rong
     temp_ε = left_side_eps.copy()
     for i in temp_ε:
@@ -89,16 +94,178 @@ def delEmptyRule(rules):
 
     return rules
 
-# delete single rule
-# Xóa quy tắc đơn
-def delSingleRule(rules, subAlphabet):
-    new_rules = rules.copy()
-
-    for i in range(len(rules)):
-        if len(rules[i][1]) < 2 and rules[i][1] in subAlphabet:
-            new_rules.pop(i)
+# Lấy ra vế phải của một quy tắc cần tìm
+def getRightSide(rules, sym):
+    ls = []
     
-    return new_rules
+    # check sentence in grammar
+    for j in rules:
+        if sym in j[0]:
+            ls = (j[1])
+
+    return ls
+
+# Removal of Unit Productions
+# Xóa quy tắc đơn
+# Quy tắc đơn có dạng A -> B, AB không thuộc bảng ký hiệu phụ
+def rmUnitRules(rules, subAlphabet):
+    # Bộ quy tắc đã được xóa đi quy tắc rỗng
+    new_rules = cp.deepcopy(rules)
+
+    # Lấy ra vế trái của các quy tắc đơn
+    ls_left_sig = []
+    for i in rules:
+        temp_right = []
+        for j in i[1]:
+            if len(j) < 2 and j in subAlphabet and i[0] in subAlphabet :
+                temp_right.append(j)
+        
+        if len(temp_right) > 0:
+            ls_left_sig.append([i[0], temp_right])
+    
+    # temp_new_rules = ls_left_sig.copy()
+    if len(ls_left_sig) == 0:
+        return new_rules
+    else: 
+        # Xoa tung quy tac don
+        for i in ls_left_sig:
+            for j in i[1]:
+                # Lấy ra vế phải từ vế trái của quy tắc đơn
+                get_right = getRightSide(rules, j)
+
+                # Thay thế vế phải của quy tắc đơn
+                k = 0
+                check = True
+                while check and k < len(rules):
+                    if i[0] == new_rules[k][0]:
+                        for gr in get_right:
+                            new_rules[k][1].append(gr)
+                        
+                        # Xóa phần vế phải của quy tắc đơn
+                        new_rules[k][1].remove(j)
+
+                        # Tìm và xóa đi quy tắc đơn trong bộ quy tắc mới (đệ quy)
+                        rmUnitRules(new_rules, subAlphabet)
+                        check = False
+                    else: k += 1
+
+        return new_rules
+
+# Loại bỏ quy tắc/biến vô sinh
+# Những biến không dẫn ra được xâu kết thúc được gọi là biến vô sinh
+def varNotTer(mainAlphabet, subAlphabet, rules, cnf):
+    new_sub = []
+    new_rules = cp.deepcopy(rules)
+    ls_ter = []
+
+    # add rules in main alphabet
+    # thêm vào quy tắc có vế phải thuộc trong bảng chữ cái chính 
+    for i in mainAlphabet:
+        for j in rules:
+            if i in j[1]:
+                ls_ter.append(j[0])
+    
+    ls_ter = sorted(list(set(ls_ter)))
+    print(ls_ter)
+
+    # Thêm vào quy tắc mà vế phải có ký tự nằm trong bảng chữ cái chính và ls ter
+    for i in rules:
+        for j in i[1]:
+            ls_split_left_rules = splitString(j)
+            check_ter = True
+            for k in ls_split_left_rules:
+                if k in mainAlphabet or k in ls_ter:
+                    pass
+                else: check_ter = False
+            if check_ter == True and i[0] not in ls_ter:
+                ls_ter.append(i[0])
+
+    # update new_rules
+    i = 0
+    while i < len(new_rules):
+
+        # Nếu bắt đầu/vế trái quy tắc là ký tự không thuộc ls_ter thì xóa khỏi bộ quy tắc
+        if new_rules[i][0] not in ls_ter:
+            new_rules.pop(i)
+        else:
+            # Kiểm tra vế phải của quy tắc
+            j = 0
+            while j < len(new_rules[i][1]):
+            # for j in range(len(new_rules[i][1])):
+                s =new_rules[i][1][j]
+                ls_split_left_rules = splitString(new_rules[i][1][j])
+                check_ter = True
+                for k in range(len(ls_split_left_rules)):
+                    if ls_split_left_rules[k] in mainAlphabet or ls_split_left_rules[k] in ls_ter:
+                        pass
+                    else: check_ter = False
+                # Nếu trong vế phải tồn tại ký tự không có trong ls_ter thì xóa bỏ
+                if check_ter == False:
+                    new_rules[i][1].pop(j)
+                    # new_rules.append(rules[i])
+                else: j += 1
+            i += 1
+
+    # Xóa bỏ các quy tắc rỗng mới
+    new_rules = rmNullRules(new_rules)
+
+    # Bảng ký hiệu phụ mới
+    new_sub = ls_ter.copy()
+
+    # update cnf
+    # Cập nhật lại cnf
+    cnf.subAlphabet = ls_ter
+    cnf.rules = new_rules
+
+    return cnf
+
+# Loại bỏ nhứng biến không dẫn đến được
+# Những biến không dẫn xuất ra được từ S được gọi là không dẫn đến được
+def nonDerivable(mainAlphabet, subAlphabet, startSymbol, rules, cnf):
+    new_sub = []
+    new_main = []
+    new_rules = []
+
+    _der = [startSymbol]
+    union_sub_main = list(set(mainAlphabet) | set(subAlphabet))
+
+    check = True
+    _der_loop = _der.copy()
+    while check:
+        temp_der = _der.copy()
+        new_der = []
+
+        for i in rules:
+            if i[0] in _der_loop:
+                for j in i[1]:
+                    ls_split = splitString(j)
+                    for k in ls_split:
+                        new_der.append(k)
+
+                        if k not in temp_der:
+                            temp_der.append(k)
+        if _der == temp_der:
+            check = False
+        else:
+            _der = temp_der
+            _der_loop = new_der
+                    
+    new_sub = list(set(subAlphabet) & set(_der))
+    new_main = list(set(mainAlphabet) & set(_der))
+
+    for i in rules:
+        if i[0] in _der:
+            new_rules.append(i)
+
+    cnf.mainAlphabet = new_main
+    cnf.subAlphabet = new_sub
+    cnf.rules = new_rules
+
+    return cnf
+# Removal of Useless rule
+# loại bỏ các qui tắc dư thừa
+def rmUselessRule():
+    pass
 
 # Lấy ra quy tắc dạng A -> a, A -> BC
 def getRulesStandard(mainAlphabet, subAlphabet, rules):
@@ -109,11 +276,23 @@ def getRulesStandard(mainAlphabet, subAlphabet, rules):
             check_sub = lenght_sym <= 2 & i[1]
             # Kiểm tra vế phải chỉ là 1 ký tự và thuộc bảng chữ cái chính
 
-
+def updateRules(mainAlphabet, subAlphabet, startSymbol, rules):
+    rules = cp.deepcopy(rules)
+    new_rules = rmNullRules(rules)
 
 # cnf_data = ICNF.cnfData('test1.txt')
-cnf_data = ICNF.cnfData('test2.txt')
+# cnf_data = ICNF.cnfData('test2.txt')
+cnf_data = ICNF.cnfData('test3.txt')
+# cnf_data = ICNF.cnfData('test4.txt')
+# cnf_data = ICNF.cnfData('test5.txt')
 # cnf_data.printCNF()
-a = delEmptyRule(cnf_data.rules)
+rules = ICNF.setType(cnf_data.rules)
+
+a = rmNullRules(rules)
 print(a)
-# delEmptyRule(cnf_data.rules)
+b = rmUnitRules(a, cnf_data.subAlphabet)
+print(b)
+c = varNotTer(cnf_data.mainAlphabet, cnf_data.subAlphabet, b, cnf_data)
+print(c)
+d = nonDerivable(c.mainAlphabet, c.subAlphabet, c.startSymbol, c.rules, c)
+d.printCNF()
