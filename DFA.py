99374e93076cc43eb12e4c659de7c8140acf2e2b
maih1 <hoangmai.tq2000@gmail.com>
2021-08-01 04:41:16 +0700

    update dfa

--- DFA.py
+++ DFA.py
@@ -323,7 +323,7 @@ if __name__ == '__main__':
     print("Automation input: ")
     automaton = inputData('./Data/DFA/testdfa1.txt')
     # automaton = inputData('./Data/DFA/testdfa2.txt')
-    automaton = inputData('./Data/NFA/testinputdfa.txt')
+    # automaton = inputData('./Data/NFA/testinputdfa.txt')
     automaton.printAutomation()
     
     print('----------------------------------------------')
